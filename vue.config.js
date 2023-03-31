79c87575283e129176ce01298909861311b9323b
Ivan Sinyavin <vanhocker@gmail.com>
2023-03-31 11:13:14 +0400

    upd

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  publicPath: '/martians/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/martians/' : '/',
   transpileDependencies: true,
 });
